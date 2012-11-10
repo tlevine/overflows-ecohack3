deae64700c4ca5952c96ff415425a5576ba6e3da
Casey Webb <caseytwebb@gmail.com>
2012-11-10 15:29:45 -0500

    date conversion stuff

--- api/munge.r
+++ api/munge.r
@@ -1,3 +1,8 @@
 #!/usr/bin/env Rscript
 
-
+o <- read.csv('overflow_dates.csv')
+o$datetime <- strptime(o$start_date, format = '%m/%d/%Y') + 3600 * o$hour
+o$hour <- NULL
+o$start_date <- NULL
+o$after.9.am <- as.numeric(strftime(o$datetime, '%H')) > 9
+o$overflow <- o$overflow == 'yes'
